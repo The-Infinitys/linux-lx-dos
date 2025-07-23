c2fa325b5aa1d14b8abc10b94a36de7ca38d19fe
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-07-23 23:01:19 +0900

    雛形作成中

--- src/qt6.rs
+++ src/qt6.rs
@@ -5,3 +5,41 @@
 mod bind {
     include!(concat!(env!("OUT_DIR"), "/qt6-bind.rs"));
 }
+#[derive(Debug, thiserror::Error)]
+pub enum Qt6Error {}
+#[derive(Debug)]
+pub struct QtApp {
+    // TODO: QtApplication構造体を作成する
+}
+impl Default for QtApp {
+    fn default() -> Self {
+        Self {}
+    }
+}
+impl QtApp {
+    pub fn new() -> Self {}
+    pub fn with_id(self, id: &str) -> Self {}
+    pub fn run(&self) -> Result<(), Qt6Error> {
+        // TODO: 起動プロセスを作成する(処理をブロック)
+        Ok(())
+    }
+    pub fn start(&self) -> Result<QtAppInstance, Qt6Error> {
+        // TODO: 開始プロセスを作成する(処理をブロックせず、後からイベントなどを受け取れるようにする)
+    }
+}
+#[derive(Debug)]
+pub struct QtAppInstance {
+    // TODO: QtAppに処理を送信したりするための機能を作成したい
+}
+
+#[derive(Debug)]
+pub enum QtAppEvent{
+    // TODO: QtAppのイベントを作成する
+}
+pub struct QtAppEventListener{
+    // TODO: イベントリスナを作成する。イベントに応じて処理をできるようにしてほしいです。
+}
+pub struct QtAppTray{
+    // TODO: トレイアイコンを表示することをできるようにしてほしいです。
+    // イベント等も実装してください。また、メニューも設定してほしいです
+}
