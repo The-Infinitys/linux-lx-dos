c600e3b3da472f5252d79d36103d22ca4400fe72
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-07-23 15:42:22 +0900

    Auto-save: Changed 17 lines in src/lib.rs src/qt6.rs  on 2025-07-23 15:42:22

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,4 @@ pub mod command;
 pub mod modules;
 pub mod utils;
 pub use utils::error::LxDosError;
-pub mod qt6 {
-    #[allow(warnings)]
-    #[allow(non_upper_case_globals)]
-    #[allow(non_camel_case_types)]
-    #[allow(non_snake_case)]
-    mod bind {
-        include!(concat!(env!("OUT_DIR"), "/qt6-bind.rs"));
-    }
-}
+pub mod qt6;
---
+++ src/qt6.rs
@@ -0,0 +1,7 @@
+#[allow(warnings)]
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+mod bind {
+    include!(concat!(env!("OUT_DIR"), "/qt6-bind.rs"));
+}
