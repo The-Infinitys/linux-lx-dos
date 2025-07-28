1fbec2b8adf60339eadfff97f5326d40ac39ccea
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-07-28 14:43:37 +0900

    Auto-save: Changed 17 lines in lib/gui/.vscode/settings.json lib/gui/src/gui_lib/qt6.rs lib/gui/src/gui_lib/qt6/bind.rs  on 2025-07-28 14:43:37

--- lib/gui/src/gui_lib/qt6.rs
+++ lib/gui/src/gui_lib/qt6.rs
@@ -1,7 +1,1 @@
-mod bind {
-    #![allow(non_upper_case_globals)]
-    #![allow(non_camel_case_types)]
-    #![allow(non_snake_case)]
-    #![allow(improper_ctypes)]
-    include!(concat!(env!("OUT_DIR"), "/qt6-bind.rs"));
-}
+mod bind;
