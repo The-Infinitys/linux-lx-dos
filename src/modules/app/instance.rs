31189636eaa462cd50457804be90eb4b9262c5da
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-08-01 11:21:43 +0900

    Auto-save: Changed 12 lines in src/modules/app/instance.rs  on 2025-08-01 11:21:43

--- src/modules/app/instance.rs
+++ src/modules/app/instance.rs
@@ -1,4 +1,14 @@
-pub struct WindowInstance {}
+use instance_pipe::{Client, Server};
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub enum InstanceMessage {}
+
+pub struct WindowServer {
+    server: Server,
+}
+pub struct WindowClient {
+    client: Client,
+}
 pub struct WindowManager {}
 impl WindowManager {
     pub fn new() -> Self {
